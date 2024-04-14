ed3ce9ca0ba4b9147798098fbe0aff44f5b0d8be
Dineth Janitha <cryptowrokdjk@gmail.com>
2024-04-14 11:01:02 +0530

    Updated project to version 1.1.0 (fix bugs)

--- index.js
+++ index.js
@@ -1,5 +1,25 @@
-const osstea = () => {
-  return console.log('Hello Tea')
+function add(a, b) {
+  return a + b
 }
 
-module.exports = osstea
+function subtract(a, b) {
+  return a - b
+}
+
+function multiply(a, b) {
+  return a * b
+}
+
+function divide(a, b) {
+  if (b === 0) {
+    throw new Error('Division by zero')
+  }
+  return a / b
+}
+
+module.exports = {
+  add,
+  subtract,
+  multiply,
+  divide,
+}
